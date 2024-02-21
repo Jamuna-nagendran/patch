8cbeed8cdb581dad0ef1307c0ab87d451f6892ee
Jamuna-nagendran <jamuna.nagendran@gmail.com>
2024-02-21 12:27:57 +0530

    Added action() to Assignment Classs in feature branch

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -5,4 +5,8 @@ public class Assignment {
         System.out.println("The sum of two integers are: " + (a+b));
     }
 
+    public static void action(){
+        System.out.println("I am eating");
+    }
+
 }
