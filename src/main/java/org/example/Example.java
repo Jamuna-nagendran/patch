93a6461086fcfa3df92202ed4cbc7d4003117d68
Jamuna-nagendran <jamuna.nagendran@gmail.com>
2024-02-21 12:23:12 +0530

    Added greeting() to Example Class

--- src/main/java/org/example/Example.java
+++ src/main/java/org/example/Example.java
@@ -4,4 +4,8 @@ public class Example {
     public void print(){
         System.out.println("Hello World!");
     }
+
+    public void greeting() {
+        System.out.println("Hello from the other side");
+    }
 }
